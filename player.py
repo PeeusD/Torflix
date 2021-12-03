94960ac9a7a54dbcf473ed3e074bdb5b9623d4eb
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-12-03 22:53:26 +0530

    initial commit

---
+++ player.py
@@ -0,0 +1,42 @@
+from torrent.torrentz import torrent_api
+import subprocess,sys
+def main():
+      
+    
+    try:  
+        results = torrent_api()
+        magnets = []
+        for key, value in results.items():
+            magnets.append(value[5])
+            print(f"⭐{key} {value[0]} >> |Leeches:{value[1]}| |Seeds:{value[2]}| |Size:{value[3]}| |Last Updated:{value[4]}| \n\n") 
+            
+        mv_choice = int(input("Enter the index of the movie which you want to stream/download: "))
+        magnet_link = magnets[mv_choice-1]
+        download = False # Default is streaming
+        stream_choice = int(input("Press 1 to stream or Press 2 to download the movie: "))
+        if stream_choice == 2:
+            download = True
+
+        webtorrent_stream(magnet_link,download)
+        
+    except Exception as e:
+        print('Error:', e)
+
+
+# Handle Streaming
+def webtorrent_stream(magnet_link,download):
+    cmd = []
+    cmd.append("webtorrent")
+    cmd.append(magnet_link)
+    if not download:
+        cmd.append('--vlc')
+    
+    if sys.platform.startswith('linux'):
+        subprocess.call(cmd)
+    elif sys.platform.startswith('win32'):
+        subprocess.call(cmd,shell=True)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ torrent/torrentz.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup  
+
+
+def torrent_api():
+
+    torrent_results = input('Enter here to search: ')
+    print(f"Searching {torrent_results} Please Wait....")
+    torrent_results.replace(' ', '+')
+    response = requests.get(f'https://torrentzeu.org/kick.php?q={torrent_results}')
+    # print(r.status_code)
+    try:
+            soup = BeautifulSoup(response.text,'html.parser')
+            contents = soup.find('table', class_ = 'table')
+            res = {}
+            
+            for content in contents.find_all('tbody'):
+                rows = content.find_all('tr')
+                # print(rows[5])
+                i=0
+                for row in rows:
+                    udt = []
+                    res_lst = []
+                    movie_name = row.find('span').text  #movie name
+                    leeches = row.find('td', attrs={ 'data-title':'Leeches'}).text # no. of leeches
+                    for seed in row.findAll('td', attrs={ 'data-title':'Last Updated'}):  #no. of seeders and last updt
+                        udt.append(seed)
+                    last_updt = (str(udt[0])).strip('<td class="age-data" data-title="Last Updated"></td>')  #last updated
+                    seeds = (str(udt[1])).strip('<td data-title="Last Updated"></td>') #seeds
+                    file_size = row.find('td', attrs={ 'data-title':'Size'}).text  #file size
+                    magnets = row.find('a').get('href') #magnet links
+                    i=i+1
+                    
+                    res_lst.extend([movie_name, leeches, seeds, file_size, last_updt, magnets])  #appending multiple values into res_lst...
+                    res[i] = res_lst
+
+            return res
+    except Exception as e:
+        #print('Result not found, Error: ',e)     
+        return e
+
